90286b4b65651ff50c7331cc5ba91fbd1a1be803
garlic0x1 <garlic1@protonmail.com>
2024-11-18 23:57:39 -0600

    redefine

--- thespis.lisp
+++ thespis.lisp
@@ -2,6 +2,8 @@
 
 (defvar *self*)
 
+(defvar *registry* (make-hash-table))
+
 (defstruct close-signal)
 
 (defstruct async-signal
@@ -11,14 +13,19 @@
   (msg nil                               :type list)
   (sem (error "Must provide :semaphore") :type bt2:semaphore))
 
-(defstruct (actor (:constructor make-actor%))
-  (behav  (error "Must provide :behav") :type function)
+(defstruct actor
+  (behav  (error "Must provide :behav") :type symbol)
   (fail   #'error                       :type function)
   (queue  (error "Must provide :queue") :type q:simple-cqueue)
   (openp  t                             :type boolean)
   (store  nil                           :type list)
   (sem    (error "Must provide :sem")   :type bt2:semaphore)
   (thread nil                           :type (or null bt2:thread)))
+
+(defun resolve-actor (actor)
+  (etypecase actor
+    (actor actor)
+    (symbol (gethash actor *registry*))))
 
 (defun process-message (actor msg)
   (let ((*self* actor))
@@ -43,43 +50,35 @@
         (null
          (bt2:wait-on-semaphore (actor-sem actor)))))))
 
-(defun make-actor (behav)
-  "Make an actor and run event loop."
-  (let ((actor (make-actor% :behav behav
-                            :sem (bt2:make-semaphore)
-                            :queue (q:make-queue :simple-cqueue))))
-    (setf (actor-thread actor) (bt2:make-thread (lambda () (run-actor actor))))
-    actor))
-
 (defun send-signal (actor sig)
   (unless (actor-openp actor)
     (error (format nil "Message sent to closed actor: ~w" actor)))
   (q:qpush (actor-queue actor) sig)
   (bt2:signal-semaphore (actor-sem actor)))
 
-(defun close-actor (actor)
+(defun close-actor (actor &aux (actor (resolve-actor actor)))
   "Send a close-signal to an actor."
   (send-signal actor (make-close-signal))
   (setf (actor-openp actor) nil))
 
-(defun join-actor (actor)
+(defun join-actor (actor &aux (actor (resolve-actor actor)))
   "Wait for an actor to finish computing."
   (bt2:join-thread (actor-thread actor))
   (apply #'values (actor-store actor)))
 
-(defun destroy-actor (actor)
+(defun destroy-actor (actor &aux (actor (resolve-actor actor)))
   "Immediately destroy an actor's thread."
   (bt2:destroy-thread (actor-thread actor)))
 
 (defun close-and-join-actors (&rest actors)
-  (mapcar #'close-actor actors)
-  (mapcar #'join-actor actors))
+  (mapc #'close-actor actors)
+  (mapc #'join-actor actors))
 
-(defun send (actor &rest args)
+(defun send (actor &rest args &aux (actor (resolve-actor actor)))
   "Asyncronously send a message to an actor."
   (send-signal actor (make-async-signal :msg args)))
 
-(defun ask (actor &rest args)
+(defun ask (actor &rest args &aux (actor (resolve-actor actor)))
   "Syncronously send a message and await a response from an actor"
   (let ((sem (bt2:make-semaphore)))
     (send-signal actor (make-sync-signal :msg args :sem sem))
@@ -87,11 +86,23 @@
     (apply #'values (actor-store actor))))
 
 (defmacro define-actor (name state args &body body)
-  "This macro creates a function named ,name that spawns an instance of
-the actor.  ,state is a let-list of mutable data owned by the
-actor. ,args is a lambda-list to destructure messages with, and ,body
+  "This macro creates a function named `name' that spawns an instance of
+the actor.  `state' is a let-list of mutable data owned by the
+actor. `args' is a lambda-list to destructure messages with, and `body'
 is the code to handle messages."
-  `(defun ,name ()
-     (let ,state
-       (labels ((me ,args ,@body))
-         (make-actor #'me)))))
+  (let ((behav (intern (string-upcase (format nil "~a-behav" name)))))
+    `(progn
+       (defun ,behav ,args
+         ,@(mapcar (lambda (pair) `(declare (special ,(car pair)))) state)
+         ,@body)
+       (defun ,name ()
+         (let ((actor (make-actor :behav ',behav
+                                  :sem (bt2:make-semaphore)
+                                  :queue (q:make-queue :simple-cqueue))))
+           (setf (actor-thread actor)
+                 (bt2:make-thread
+                  (lambda ()
+                    (let ,state
+                      ,@(mapcar (lambda (pair) `(declare (special ,(car pair)))) state)
+                      (run-actor actor)))))
+           actor)))))
